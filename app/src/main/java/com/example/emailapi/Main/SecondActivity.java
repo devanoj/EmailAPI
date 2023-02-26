e7d335966fb308743846bc2dfb041b9912f6d10e
Devano <C19312821@mytudublin.ie>
2023-02-26 20:36:51 +0000

    Commit 3

--- app/src/main/java/com/example/emailapi/Main/SecondActivity.java
+++ app/src/main/java/com/example/emailapi/Main/SecondActivity.java
@@ -1,60 +1,34 @@
 package com.example.emailapi.Main;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 
 import android.util.Log;
 import android.view.View;
-import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.emailapi.DAO.UserDAO;
-import com.example.emailapi.Entity.Retrieve;
 import com.example.emailapi.Entity.User;
 
 
-import com.example.emailapi.Image.Upload;
 import com.example.emailapi.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.StorageTask;
-import com.google.firebase.storage.UploadTask;
-
-import org.w3c.dom.Text;
-
-import java.util.Properties;
-
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-
+import com.google.firebase.database.ValueEventListener;
 
 
 public class SecondActivity extends AppCompatActivity {
@@ -95,26 +69,21 @@ public class SecondActivity extends AppCompatActivity {
         inputFT = findViewById(R.id.freeTimeEdit);
         inputLS = findViewById(R.id.lifestyleEdit);
 
+        viewEmail = findViewById(R.id.textView2);
+
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this,gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this); // Not using this feature anymore
 
 
         if (currentUser != null) {
             email1 = currentUser.getEmail();
             userId = currentUser.getUid();
-        } else {
-            email1 = account.getEmail();
         }
-
-       /**
-        ^^^ Deal with this, mak sure you only use firebase au **/
 
         if (bundle != null) {
             String email = bundle.getString("email");
             String password = bundle.getString("password");
-            viewEmail = findViewById(R.id.textView2);
-            TextView viewpassword = findViewById(R.id.textView2);
             viewEmail.setText(email);
         }
 
@@ -148,21 +117,7 @@ public class SecondActivity extends AppCompatActivity {
         });
 
         infoButton.setOnClickListener(view -> {
-            if (isEditTextVisible) {
-                inputName.setVisibility(View.INVISIBLE);
-                inputAge.setVisibility(View.INVISIBLE);
-                inputFT.setVisibility(View.INVISIBLE);
-                inputLS.setVisibility(View.INVISIBLE);
-                submit1.setVisibility(View.INVISIBLE);
-                isEditTextVisible = false;
-            } else {
-                inputName.setVisibility(View.VISIBLE);
-                inputAge.setVisibility(View.VISIBLE);
-                inputFT.setVisibility(View.VISIBLE);
-                inputLS.setVisibility(View.VISIBLE);
-                submit1.setVisibility(View.VISIBLE);
-                isEditTextVisible = true;
-            }
+            isItOrg(userId);
         });
 
 
@@ -203,6 +158,65 @@ public class SecondActivity extends AppCompatActivity {
         }); */
 
 
+    }
+
+    private void isItOrg(String userId) {
+        dr = FirebaseDatabase.getInstance().getReference("User");
+
+        dr.child(userId).child("organisation").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean x = Boolean.TRUE.equals(snapshot.getValue(boolean.class));
+                updateUser(x);;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Email Database Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateUser(boolean x) {
+        /**if (isEditTextVisible) {
+            inputName.setVisibility(View.INVISIBLE);
+            inputAge.setVisibility(View.INVISIBLE);
+            inputFT.setVisibility(View.INVISIBLE);
+            inputLS.setVisibility(View.INVISIBLE);
+            submit1.setVisibility(View.INVISIBLE);
+            isEditTextVisible = false;
+        }
+        else {
+            inputName.setVisibility(View.VISIBLE);
+            inputAge.setVisibility(View.VISIBLE);
+            inputFT.setVisibility(View.VISIBLE);
+            inputLS.setVisibility(View.VISIBLE);
+            submit1.setVisibility(View.VISIBLE);
+            isEditTextVisible = true;
+        } **/
+
+        if (!isEditTextVisible && !x) {
+            inputName.setVisibility(View.VISIBLE);
+            inputAge.setVisibility(View.VISIBLE);
+            inputFT.setVisibility(View.VISIBLE);
+            inputLS.setVisibility(View.VISIBLE);
+            submit1.setVisibility(View.VISIBLE);
+            isEditTextVisible = true;
+        } else if (!isEditTextVisible && x) {
+            inputName.setVisibility(View.VISIBLE);
+            inputAge.setVisibility(View.INVISIBLE); // Important
+            inputFT.setVisibility(View.VISIBLE);
+            inputLS.setVisibility(View.VISIBLE);
+            submit1.setVisibility(View.VISIBLE);
+            isEditTextVisible = true;
+        } else if (isEditTextVisible) {
+            inputName.setVisibility(View.INVISIBLE);
+            inputAge.setVisibility(View.INVISIBLE);
+            inputFT.setVisibility(View.INVISIBLE);
+            inputLS.setVisibility(View.INVISIBLE);
+            submit1.setVisibility(View.INVISIBLE);
+            isEditTextVisible = false;
+        }
     }
 
     private void deleteUser() {
