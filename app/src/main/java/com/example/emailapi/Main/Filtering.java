3d1c0610fb20d2735c882730c5aaa19b3dff0624
Devano <C19312821@mytudublin.ie>
2023-03-01 10:10:34 +0000

    Commit 9 - Filtering Activity added DogAPI() & makeRequest() functions

--- app/src/main/java/com/example/emailapi/Main/Filtering.java
+++ app/src/main/java/com/example/emailapi/Main/Filtering.java
@@ -35,6 +35,7 @@ public class Filtering extends AppCompatActivity {
     ImageView animalExplore, Profile1, Filter1, Find1, HomeMain1;
 
     String outputSim;
+    String x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +45,37 @@ public class Filtering extends AppCompatActivity {
         getRequest = findViewById(R.id.GetRequest);
         EnterDogName = findViewById(R.id.Similar);
 
+
+
         sideNavMenu();
-        getRequest();
-        new DogAPI().execute();
+        //getRequest();
+        executeDogAPI();
+
+    }
+
+    private void executeDogAPI() {
+        getRequest.setOnClickListener(v->{
+            x = EnterDogName.getText().toString();
+            new DogAPI(x).execute();
+            makeRequest(x);
+        });
 
     }
 
     private class DogAPI extends AsyncTask<Void, Void, String> {
+        private String x;
+
+        public DogAPI(String x) {
+            this.x = x;
+        }
 
         @Override
         protected String doInBackground(Void... params) {
             try {
                 // Set the API endpoint and API key
-                String endpoint = "https://api.api-ninjas.com/v1/dogs?name=briard";
+                String link = "https://api.api-ninjas.com/v1/dogs?name=";
+                String nameOfBreed = "briard";
+                String endpoint = link + this.x;
                 String apiKey = "BI+Alyvlya8ICTKjNAny1w==zTNZj4XYZz6ESIVw";
 
                 // Make the API call with the API key as a request header
@@ -86,48 +105,123 @@ public class Filtering extends AppCompatActivity {
             if (response != null) {
                 try {
                     JSONArray jsonArray = new JSONArray(response);
-                    JSONObject jsonObject = jsonArray.getJSONObject(0);
+                    if (jsonArray.length() > 0) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(0);
+                        String name = jsonObject.getString("name");
+                        int goodWithChildren = jsonObject.getInt("good_with_children");
+                        int goodWithOtherDogs = jsonObject.getInt("good_with_other_dogs");
+                        int shedding = jsonObject.getInt("shedding");
+                        int grooming = jsonObject.getInt("grooming");
+                        int drooling = jsonObject.getInt("drooling");
+                        int coatLength = jsonObject.getInt("coat_length");
+                        int goodWithStrangers = jsonObject.getInt("good_with_strangers");
+                        int playfulness = jsonObject.getInt("playfulness");
+                        int protectiveness = jsonObject.getInt("protectiveness");
+                        int trainability = jsonObject.getInt("trainability");
+                        int energy = jsonObject.getInt("energy");
+                        int barking = jsonObject.getInt("barking");
+                        int minLifeExpectancy = jsonObject.getInt("min_life_expectancy");
+                        int maxLifeExpectancy = jsonObject.getInt("max_life_expectancy");
 
-                    String name = jsonObject.getString("name");
-                    int goodWithChildren = jsonObject.getInt("good_with_children");
-                    int goodWithOtherDogs = jsonObject.getInt("good_with_other_dogs");
-                    int shedding = jsonObject.getInt("shedding");
-                    int grooming = jsonObject.getInt("grooming");
-                    int drooling = jsonObject.getInt("drooling");
-                    int coatLength = jsonObject.getInt("coat_length");
-                    int goodWithStrangers = jsonObject.getInt("good_with_strangers");
-                    int playfulness = jsonObject.getInt("playfulness");
-                    int protectiveness = jsonObject.getInt("protectiveness");
-                    int trainability = jsonObject.getInt("trainability");
-                    int energy = jsonObject.getInt("energy");
-                    int barking = jsonObject.getInt("barking");
-                    int minLifeExpectancy = jsonObject.getInt("min_life_expectancy");
-                    int maxLifeExpectancy = jsonObject.getInt("max_life_expectancy");
 
-
-                    TextView textView = findViewById(R.id.DogAPI);
-                    String text = "Information about dog breed" + "\n" +
-                            "Name: " + name + "\n" +
-                            "Good with children: " + goodWithChildren + "\n" +
-                            "Good with other dogs: " + goodWithOtherDogs + "\n" +
-                            "Shedding: " + shedding + "\n" +
-                            "Grooming: " + grooming + "\n" +
-                            "Drooling: " + drooling + "\n" +
-                            "Coat length: " + coatLength + "\n" +
-                            "Good with strangers: " + goodWithStrangers + "\n" +
-                            "Playfulness: " + playfulness + "\n" +
-                            "Protectiveness: " + protectiveness + "\n" +
-                            "Trainability: " + trainability + "\n" +
-                            "Energy: " + energy + "\n" +
-                            "Barking: " + barking + "\n" +
-                            "Minimum life expectancy: " + minLifeExpectancy + "\n" +
-                            "Maximum life expectancy: " + maxLifeExpectancy;
-                    textView.setText(text);
+                        TextView textView = findViewById(R.id.DogAPI);
+                        String text = "Information about dog breed" + "\n" +
+                                "Name: " + name + "\n" +
+                                "Good with children: " + goodWithChildren + "\n" +
+                                "Good with other dogs: " + goodWithOtherDogs + "\n" +
+                                "Shedding: " + shedding + "\n" +
+                                "Grooming: " + grooming + "\n" +
+                                "Drooling: " + drooling + "\n" +
+                                "Coat length: " + coatLength + "\n" +
+                                "Good with strangers: " + goodWithStrangers + "\n" +
+                                "Playfulness: " + playfulness + "\n" +
+                                "Protectiveness: " + protectiveness + "\n" +
+                                "Trainability: " + trainability + "\n" +
+                                "Energy: " + energy + "\n" +
+                                "Barking: " + barking + "\n" +
+                                "Minimum life expectancy: " + minLifeExpectancy + "\n" +
+                                "Maximum life expectancy: " + maxLifeExpectancy;
+                        textView.setText(text);
+                    } else {
+                        TextView textView = findViewById(R.id.DogAPI);
+                        String text = "No results found.";
+                        textView.setText(text);
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         }
+
+
+    }
+
+    private void makeRequest(String y) {
+        try {
+            String baseUrl = "http://10.0.2.2:8000/getRecommendation/";
+
+
+            DogName = findViewById(R.id.DogName);
+
+                //String queryParameter = EnterDogName.getText().toString();
+                String link = baseUrl + y;
+                RequestQueue queue = Volley.newRequestQueue(this);
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, link, null, response -> {
+                            try {
+                                if (response.has("sim")) {
+                                    JSONObject z = response.getJSONObject("sim");
+                                    outputSim = z.toString();
+
+
+                                    // Remove the brackets and quotes
+                                    String cleanInput = outputSim.replace("{", "").replace("}", "");
+
+                                    // Split the input into breed names and scores
+                                    String[] breedScores = cleanInput.split(",");
+
+                                    // Initialize a StringBuilder to store the breed names and scores
+                                    StringBuilder resultBuilder = new StringBuilder();
+
+                                    // Loop through each breed score and convert the decimal to a percentage
+                                    for (String breedScore : breedScores) {
+                                        String[] parts = breedScore.split(":");
+                                        String breedName = parts[0];
+                                        double score = Double.parseDouble(parts[1]);
+                                        int percentage = (int) (score * 100);
+                                        String formattedScore = percentage + "%";
+                                        String breedResult = breedName + ": " + formattedScore;
+                                        resultBuilder.append(breedResult).append("\n");
+                                    }
+
+                                    // Get the final result as a string
+                                    String result = resultBuilder.toString();
+                                    DogName.setText(result);
+                                    //DogName.setText(x.toString());
+
+                                } else {Toast.makeText(Filtering.this, "Key 'good_with_children' not found in response", Toast.LENGTH_SHORT).show();}
+                            } catch (JSONException e) {Toast.makeText(Filtering.this, "Error parsing JSON: " + e.getMessage(), Toast.LENGTH_SHORT).show();}
+                        }, error -> {
+                            try {
+                                String responseBody = new String(error.networkResponse.data, "utf-8");
+                                JSONObject data = new JSONObject(responseBody);
+                                String message = data.getString("message");
+                                Toast.makeText(Filtering.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                            } catch (JSONException e) {
+                                Toast.makeText(Filtering.this, "Dog doesn't exist in the database", Toast.LENGTH_SHORT).show();
+                            } catch (UnsupportedEncodingException e) {
+                                Toast.makeText(Filtering.this, "Unsupported encoding: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                queue.add(jsonObjectRequest);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Info not found", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void getRequest() {
