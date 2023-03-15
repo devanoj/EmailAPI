98d14092167281a05c8adf30a937a45e08ea2773
Devano <C19312821@mytudublin.ie>
2023-03-15 15:43:37 +0000

    Commit 20 - Cleaned layout for add_info_layout.xml & getValue from Spinner

--- app/src/main/java/com/example/emailapi/Main/AddInfo.java
+++ app/src/main/java/com/example/emailapi/Main/AddInfo.java
@@ -1,28 +1,42 @@
 package com.example.emailapi.Main;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.emailapi.R;
 
 public class AddInfo extends AppCompatActivity {
+
+    Button nextPage;
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_info_layout);
 
-        Spinner spinner = findViewById(R.id.my_spinner);
+        spinner = findViewById(R.id.my_spinner);
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.my_array, android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        goToLogin();
+
+    }
+
+    private void goToLogin() {
+        nextPage = findViewById(R.id.nextPage);
+        nextPage.setOnClickListener(v->{
+            String selectedItem = spinner.getSelectedItem().toString();
+            Log.d("ItemChosenXXX", "Selected item: " + selectedItem);
+        });
     }
 }
